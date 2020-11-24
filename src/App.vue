f08ee00a347c6063310673a8bce05f8f62b02d23
delinuxist <delinuxist@gmail.com>
2020-11-24 00:46:31 +0000

    main vue app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,45 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <appHeader :maxQuotes="maxQuotes" :quoteCount="quotes.length"></appHeader>
+    <newQuote @quoteAdded="newQuote"></newQuote>
+    <quoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></quoteGrid>
+
+    <div class="row mt-5">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to Delete it!</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+export default {
+  data: function () {
+    return {
+      quotes: ["Just a Quote to see something"],
+      maxQuotes: 10,
+    };
+  },
+  components: {
+    quoteGrid: QuoteGrid,
+    newQuote: NewQuote,
+    appHeader: Header,
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please Delete Quotes first!");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
