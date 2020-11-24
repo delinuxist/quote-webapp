29f45121f47d68a33e6d2da73b5b472c5dae353f
delinuxist <delinuxist@gmail.com>
2020-11-24 00:47:02 +0000

    newquote file

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row mt-5">
+    <div class="col-sm-8 col-7 mx-auto">
+      <form>
+        <div class="form-group">
+          <label class="font-weight-bolder">Quote</label>
+          <textarea class="form-control" rows="3" v-model="quote"></textarea>
+        </div>
+      </form>
+    </div>
+
+    <div class="col-sm-8 col-7 mx-auto">
+      <form>
+        <div class="form-group">
+          <button class="btn btn-primary" @click.prevent="createNew">
+            Add Quote
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      quote: "",
+    };
+  },
+  methods: {
+    createNew() {
+      this.$emit("quoteAdded", this.quote);
+      this.quote = "";
+    },
+  },
+};
+</script>
+<style>
+</style>
